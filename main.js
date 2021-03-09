1114092d1ab00cc91f136c4c14d3d56b747f3eed
joseee177 <josezuniga1517@gmail.com>
2021-03-08 21:19:45 -0600

    pushing up piglatin

--- main.js
+++ main.js
@@ -12,10 +12,78 @@ const rl = readline.createInterface({
 
 
 const pigLatin = (word) => {
+  let cleanWord = word.trim().toLowerCase();
 
-  // Your code here
+  let translation = "";
+  
+  let smallestNonNegative = (num1,num2) => {
+    //if num1 is negative, return num2
+      if(num1 < 0){
+        return num2;
+      }
+    // if num2 is negative, return num1
+      if(num2 < 0){
+        return num1;
+      }
+    //otherwise, return the smaller of the two
+      if(num1 < num2){
+        return num1;
+      } else{
+        return num2;
+      }
+    }
+
+    let indexOfFirstVowel = (word) =>{
+      let vowelIndex = -1; //to set as default
+
+      //indexOf --> finds where the character appears is in the word.. in this case vowels
+      //indexOf will give us the position of the vowel in a numeric value
+      const aIndex = word.indexOf("a");
+      const eIndex = word.indexOf("e");
+      const iIndex = word.indexOf("i");
+      const oIndex = word.indexOf("o");
+      const uIndex = word.indexOf("u");
+   
+
+      //reasigning the vale of the variable vowelIndex
+     vowelIndex = smallestNonNegative(vowelIndex, aIndex);
+     vowelIndex = smallestNonNegative(vowelIndex, eIndex);
+     vowelIndex = smallestNonNegative(vowelIndex, iIndex);
+     vowelIndex = smallestNonNegative(vowelIndex, oIndex);
+     vowelIndex = smallestNonNegative(vowelIndex, uIndex);
+     
+     return vowelIndex;
+    }
+//created a variable named vowel which equals indexOfFirstVowel with cleanWord passed into it to trim and lowercase the vowel/word
+    let vowel = indexOfFirstVowel(cleanWord);
+
+  //if word begins with a vowel
+  if(vowel == 0){
+    translation = cleanWord + "yay";
+
+    return translation;
+  } 
+   //if the word doesnt start with a vowel
+  else{
+    //.substring helps to separate the word from where the first vowel is found
+    translation = cleanWord.substring(vowel) + cleanWord.substring(0,vowel) + "ay";
+
+    return translation;
+  } 
+  
 
 }
+/****************************************************************************************************/
+let inputButton = document.getElementById("inputButton")
+inputButton.addEventListener ('click', function(){
+
+  //we call the input element from the DOM
+  let inputBox = document.getElementById("inputBox");
+
+  let span = document.getElementById("translation");
+
+  span.innerText = pigLatin(inputBox.value);
+})
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
